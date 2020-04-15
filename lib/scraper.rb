d67787d93c70a73e80ff4eb98d13c9f19ba170e3
Jack Madden <jck13mad@gmail.com>
2020-04-15 21:03:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,20 +21,20 @@ class Scraper
       
 
   def self.scrape_profile_page(profile_url)
-    student_profile = {}
-    html = open(profile_url)
-    profile = Nokogiri::HTML(html)
     
-    profile.css("div.main_wrapper.profile .social_icon_container a").each do |social|
+    
+    
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
       if social.attribute("href").value.include?("twitter")
         student_profile[:twitter] = social.attribute("href").value
       elsif social.attribute("href").value.include?("linkedin")
-        student
-    
-    
-    
-    
-  end
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
 
 end
 
