5af4125f34c3b7e9b91ef18c01ea7e28157e552c
Jack Madden <jck13mad@gmail.com>
2020-04-15 20:55:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    index.css("div.student_card").each do |student|
   end
 
   def self.scrape_profile_page(profile_url)
